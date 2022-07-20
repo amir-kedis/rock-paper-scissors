65e7558cae6e29d54d3d1756198778320cd98046
amir-kedis <amir.kedis@gmail.com>
2022-07-20 11:02:43 +0200

    make getComputerChoice function

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log("HI there!");
+/* takes no input
+* returns a random choice of ['r', 'p', 's']
+*/
+function getComputerChoice() {
+    let choices = ['r', 'p', 's'];
+    let randomNum = Math.floor(Math.random() * 3);
+    return choices[randomNum];
+}
