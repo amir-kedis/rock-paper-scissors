5239cb46e72a23749c0fd6cc3ae39b3828c1bec1
amir-kedis <amir.kedis@gmail.com>
2022-07-20 11:19:50 +0200

    make userSelection function

--- main.js
+++ main.js
@@ -6,3 +6,24 @@ function getComputerChoice() {
     let randomNum = Math.floor(Math.random() * 3);
     return choices[randomNum];
 }
+
+/* takes no input
+* returns user choice ['r', 'p', 's'] 
+* asks user for his input 
+* if he enters a proper answer return its abbreviation
+* else it calls itself again and asks user again
+*/
+function userSelection() 
+{
+    let input = prompt("enter your choice? humanity depends on you");
+
+    if (input.toUpperCase() == "ROCK") {
+        return 'r';
+    } else if (input.toUpperCase() == "PAPER") {
+        return 'p';
+    } else if (input.toUpperCase() == "SCISSORS") {
+        return 's';
+    } else {
+        return userSelection();
+    }
+}
